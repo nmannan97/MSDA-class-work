b6251ff7f583e61116a8e68a5281ee6faf8385bc
Naeem Mannan <nmannan97@gmail.com>
2025-02-01 09:51:24 -0800

    adding finishing things

--- DATA-200/Mannan_HW1.py
+++ DATA-200/Mannan_HW1.py
@@ -1,16 +1,20 @@
 import sys
 import os
 from PyQt5.QtWidgets import *
+import json
 
 # Define error window
 class errorWindow(QWidget):
-    def __init__(self):
+    def __init__(self, text=""):
         super().__init__()
         self.setWindowTitle("Error")
         self.resize(300, 200)
 
         layout = QVBoxLayout()
-        label = QLabel("ERROR! Try again.")
+        if text == "":
+            label = QLabel("ERROR! Try again.")
+        else:
+            label = QLabel("{}".format(text))
         layout.addWidget(label)
         self.setLayout(layout)
 
@@ -65,6 +69,59 @@ class customerWindow(QWidget):
         # Show a message box with the selected option
         QMessageBox.information(self, "Selected Option", f"You selected: {selected_option}")
 
+# Define admin ID window
+class AdminIDWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Login admin ID")
+        self.resize(400, 300)
+
+        # Create a text input field
+        self.adminid_input = QLineEdit()
+        self.adminid_input.setPlaceholderText("Enter admin ID")
+
+        # Create a button to open the customer window
+        self.button = QPushButton("submit")
+        self.button.clicked.connect(self.open_admin_window)
+
+        # Set layout
+        central_widget = QWidget()
+        layout = QVBoxLayout()
+        layout.addWidget(self.adminid_input)
+        layout.addWidget(self.button)
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+    def open_admin_window(self):
+        adminid = self.adminid_input.text()
+        if "001" in adminid:
+            self.customer_window = AdminPortalWindow()
+            self.customer_window.show()
+        else:
+            self.customer_window = errorWindow("Error!\nInvalid admin ID entered")
+            self.customer_window.show()
+
+# Define admin ID window
+class AdminPortalWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Admin portal")
+        self.resize(400, 300)
+
+        label = QLabel("Welcome to the admin portal")
+        LoginFailLabel = QLabel("Failed Logins")
+        LoginPassLabel = QLabel("Successful Logins")
+
+        # Set layout
+        central_widget = QWidget()
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+        layout.addWidget(LoginFailLabel)
+        layout.addWidget(LoginPassLabel)
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+
 # Define the main window
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -91,22 +148,27 @@ class MainWindow(QMainWindow):
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
 
+        
 
         self.customer_window = None  # Initialize customer window as None
 
     def open_customer_window(self):
         username = self.username_input.text()
         password = self.password_input.text()
-        if "customer" in username and password=="12345":
+        if username == "customer" and password=="12345":
             self.customer_window = customerWindow()
             self.customer_window.show()
+        elif username == "admin1" and password == "12345":
+            self.customer_window = AdminIDWindow()
+            self.customer_window.show()
         else:
-            self.customer_window = errorWindow()
+            self.customer_window = errorWindow(text="Error!\nInvalid username/password")
             self.customer_window.show()
 
 # Run the application
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    main_win = customerWindow() #Change to login once done
+    main_win = AdminPortalWindow()
+    #main_win = MainWindow() #Change to login once done
     main_win.show()
     sys.exit(app.exec_())
