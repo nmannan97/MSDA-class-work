4a768d18b322bdf887438460d768ea59dde7e65e
Naeem Mannan <nmannan97@gmail.com>
2025-02-03 16:17:25 -0800

    fixing big problem

--- DATA-200/Mannan_HW1.py
+++ DATA-200/Mannan_HW1.py
@@ -1,233 +1,200 @@
 import sys
 from PyQt5.QtWidgets import *
 
-# Define error window
-class assignment1():
-
+class Assignment1:
     def __init__(self):
         self.customers = {
-                            "customer 1":{
-                                "password":"12345", 
-                                "ratings":{
-                                    "TV": 0,
-                                    "Radio" : 0, 
-                                    "mobile phone": 0, 
-                                    "laptop": 0
-                                }
-                            }
-                        }
-        self.admins ={
-                        "admin1": 
-                        {
-                            "password": "12345", 
-                            "ID": "001",
-                            "failedLogins": 0,
-                            'successfulLogins': 0
-                        }, 
-                        "admin2": 
-                        {
-                            "password": "12345", 
-                            "ID": "002",
-                            "failedLogins": 0,
-                            'successfulLogins': 0
-                        }
-                    }
+            "customer1": {
+                "password": "12345",
+                "ratings": {
+                    "TV": 0,
+                    "Radio": 0,
+                    "mobile phone": 0,
+                    "laptop": 0
+                }
+            }
+        }
+        self.admins = {
+            "admin1": {
+                "password": "12345",
+                "ID": "001",
+                "failedLogins": 0,
+                "successfulLogins": 0
+            },
+            "admin2": {
+                "password": "12345",
+                "ID": "002",
+                "failedLogins": 0,
+                "successfulLogins": 0
+            }
+        }
 
-    class errorWindow(QWidget):
+    class ErrorWindow(QWidget):
         def __init__(self, text=""):
             super().__init__()
             self.setWindowTitle("Error")
             self.resize(300, 200)
 
             layout = QVBoxLayout()
-            if text == "":
-                label = QLabel("ERROR! Try again.")
-            else:
-                label = QLabel("{}".format(text))
+            label = QLabel(text if text else "ERROR! Try again.")
             layout.addWidget(label)
             self.setLayout(layout)
 
-    # Define the customer window
-    class customerWindow(QWidget):
-        def __init__(self, parents_instance):
+    class CustomerWindow(QWidget):
+        def __init__(self, parent_instance):
             super().__init__()
-            self.setWindowTitle("Admin ID window")
+            self.setWindowTitle("Customer Rating")
             self.resize(450, 350)
-            self.items = {"TV":0, "Radio":0, "mbile phone":0, "laptop":0}
-            self.data = parents_instance.customers
+            self.data = parent_instance.customers  # Store reference to main data
 
-            # Set dropdown menu
+            # Dropdown menu
             self.dropdown = QComboBox()
-            self.dropdown.addItems(self.items)  # Add options
+            self.dropdown.addItems(["TV", "Radio", "mobile phone", "laptop"])
 
-            # Define radio buttons
-            self.radio1 = QRadioButton("1 rating")
-            self.radio2 = QRadioButton("2 rating")
-            self.radio3 = QRadioButton("3 rating")
-            self.radio4 = QRadioButton("4 rating")
-            self.radio5 = QRadioButton("5 rating")
+            # Radio buttons for ratings
+            self.rating_group = QButtonGroup(self)
+            self.radio_buttons = []
+            for i in range(1, 6):
+                btn = QRadioButton(f"{i} stars")
+                self.rating_group.addButton(btn, i)
+                self.radio_buttons.append(btn)
 
-            # Create a button to display the selected option
-            self.button = QPushButton("Show Selected Option")
-            self.button.clicked.connect(self.show_selected)
+            # Button to submit rating
+            self.button = QPushButton("Submit Rating")
+            self.button.clicked.connect(self.submit_rating)
 
+            # Layout setup
             layout = QVBoxLayout()
+            layout.addWidget(QLabel("Select Product:"))
             layout.addWidget(self.dropdown)
-            layout.addWidget(self.radio1)
-            layout.addWidget(self.radio2)
-            layout.addWidget(self.radio3)
-            layout.addWidget(self.radio4)
-            layout.addWidget(self.radio5)
+            layout.addWidget(QLabel("Rate the Product:"))
+            for btn in self.radio_buttons:
+                layout.addWidget(btn)
             layout.addWidget(self.button)
             self.setLayout(layout)
 
-        def show_selected(self):
-            # Check which radio button is selected
-            for key in self.data:
-                print(self.data[key])
-            if self.radio1.isChecked():
-                selected_option = "1 stars"
-                self.data['customer 1']["ratings"][self.dropdown.currentText()] = 1
-            elif self.radio2.isChecked():
-                selected_option = "2 stars"
-                self.data['customer 1']["ratings"][self.dropdown.currentText()] = 2
-            elif self.radio3.isChecked():
-                selected_option = "3 stars"
-                self.data['customer 1']["ratings"][self.dropdown.currentText()] = 3
-            elif self.radio4.isChecked():
-                selected_option = "4 stars"
-                self.data['customer 1']["ratings"][self.dropdown.currentText()] = 4
-            elif self.radio5.isChecked():
-                selected_option = "5 stars"
-                self.data['customer 1']["ratings"][self.dropdown.currentText()] = 5
-            else:
-                selected_option = "No option selected"
+        def submit_rating(self):
+            selected_product = self.dropdown.currentText()
+            selected_rating = self.rating_group.checkedId()  # Get selected rating
 
-            # Show a message box with the selected option
-            QMessageBox.information(self, "Selected Option", f"You selected: {selected_option}")
+            if selected_rating == -1:
+                QMessageBox.warning(self, "Error", "Please select a rating!")
+                return
 
-    # Define admin ID window
+            # Store rating
+            self.data["customer 1"]["ratings"][selected_product] = selected_rating
+            QMessageBox.information(self, "Success", f"You rated {selected_product} as {selected_rating} stars!")
+
     class AdminIDWindow(QMainWindow):
-        def __init__(self, parents_instance):
+        def __init__(self, parent_instance):
             super().__init__()
-            self.setWindowTitle("Login admin ID")
-            self.resize(400, 300)
+            self.setWindowTitle("Admin Login")
+            self.resize(400, 200)
+            self.parent_instance = parent_instance  # Store reference
 
-            # Create a text input field
             self.adminid_input = QLineEdit()
-            self.adminid_input.setPlaceholderText("Enter admin ID")
+            self.adminid_input.setPlaceholderText("Enter Admin ID")
+            self.button = QPushButton("Submit")
+            self.button.clicked.connect(self.verify_admin)
 
-            # Create a button to open the customer window
-            self.button = QPushButton("submit")
-            self.button.clicked.connect(self.open_admin_window)
-
-            # Set layout
-            central_widget = QWidget()
             layout = QVBoxLayout()
             layout.addWidget(self.adminid_input)
             layout.addWidget(self.button)
+            central_widget = QWidget()
             central_widget.setLayout(layout)
             self.setCentralWidget(central_widget)
 
-        def open_admin_window(self):
-            adminid = self.adminid_input.text()
-            if "001" in adminid:
-                window_instanceAdmin = assignment1()
-                self.customer_window = window_instanceAdmin.AdminPortalWindow(window_instanceAdmin, adminid="001")
-                self.customer_window.show()
-            else:
-                self.customer_window = assignment1.errorWindow("Error!\nInvalid admin ID entered")
-                self.customer_window.show()
+        def verify_admin(self):
+            admin_id = self.adminid_input.text()
+            for admin, details in self.parent_instance.admins.items():
+                if details["ID"] == admin_id:
+                    self.parent_instance.admins[admin]['successfulLogins'] += 1
+                    self.admin_window = Assignment1.AdminPortalWindow(self.parent_instance, admin_id)
+                    self.admin_window.show()
+                    self.close()
+                    return
+                else:
+                    self.parent_instance.admins[admin]['failedLogins'] += 1
+            QMessageBox.warning(self, "Error", "Invalid Admin ID!")
 
-    # Define admin ID window
     class AdminPortalWindow(QMainWindow):
-        def __init__(self, parents_instance, adminid:int):
+        def __init__(self, parent_instance, admin_id):
             super().__init__()
-            self.setWindowTitle("Admin portal")
-            self.resize(400, 300)
-            self.data = {**parents_instance.admins, **parents_instance.customers}
-            labels = {}
-            logins = {}
-            for keys in self.data:
-                if "admin" in keys:
-                    for key in self.data[keys]:
-                        #print(self.data[keys]["admin id"])
-                        if self.data[keys]['ID'] == adminid:
-                            logins["admin"] = keys
-                            logins['failedLogins'] = self.data[keys]['failedLogins']
-                            logins['successfulLogins'] = self.data[keys]['successfulLogins']
-                    continue
-                temp_string = "{} rating\n".format(keys)
-                for key in self.data[keys]["ratings"]:
-                    print(key)
-                    temp_string += "Customer rating for {}={}\n".format(key, str(self.data[keys]["ratings"][key]))
-                labels[keys] = QLabel(temp_string)
+            self.setWindowTitle("Admin Portal")
+            self.resize(400, 400)
+            self.data = parent_instance.customers  # Get customer data
+            self.admin_data = parent_instance.admins
 
-            label = QLabel("Welcome to the admin portal, admin: {}".format(logins['admin']))
-            LoginFailLabel = QLabel("Failed Logins: {}".format(logins['failedLogins']))
-            LoginPassLabel = QLabel("Successful Logins: {}".format(logins['successfulLogins']))
+            # Get login details
+            admin_name = [name for name, details in self.admin_data.items() if details["ID"] == admin_id][0]
+            failed_logins = self.admin_data[admin_name]["failedLogins"]
+            successful_logins = self.admin_data[admin_name]["successfulLogins"]
 
-            # Set layout
-            central_widget = QWidget()
+            # Labels for login stats
+            label = QLabel(f"Welcome Admin: {admin_name}")
+            login_fail_label = QLabel(f"Failed Logins: {failed_logins}")
+            login_pass_label = QLabel(f"Successful Logins: {successful_logins}")
+
+            # Display customer ratings
+            ratings_labels = []
+            for customer, info in self.data.items():
+                text = f"{customer} Ratings:\n" + "\n".join(f"{item}: {rating}" for item, rating in info["ratings"].items())
+                ratings_labels.append(QLabel(text))
+
             layout = QVBoxLayout()
             layout.addWidget(label)
-            layout.addWidget(LoginFailLabel)
-            layout.addWidget(LoginPassLabel)
-            for item in labels:
-                layout.addWidget(labels[item])
+            layout.addWidget(login_fail_label)
+            layout.addWidget(login_pass_label)
+            for lbl in ratings_labels:
+                layout.addWidget(lbl)
+
+            central_widget = QWidget()
             central_widget.setLayout(layout)
             self.setCentralWidget(central_widget)
 
-
-    # Define the main window
     class MainWindow(QMainWindow):
-
-        def __init__(self):
+        def __init__(self, parent_instance):
             super().__init__()
-            self.setWindowTitle("Login customer/admin")
-            self.resize(400, 300)
-            
-            # Create a text input field
+            self.setWindowTitle("Login")
+            self.resize(400, 200)
+            self.parent_instance = parent_instance  # Store reference
+
+            # Input fields
             self.username_input = QLineEdit()
             self.password_input = QLineEdit()
             self.username_input.setPlaceholderText("Enter username")
             self.password_input.setPlaceholderText("Enter password")
+            self.password_input.setEchoMode(QLineEdit.Password)  # Hide password input
 
-            # Create a button to open the customer window
-            self.button = QPushButton("submit")
-            self.button.clicked.connect(self.open_window)
+            # Submit button
+            self.button = QPushButton("Submit")
+            self.button.clicked.connect(self.authenticate)
 
-            # Set layout
-            central_widget = QWidget()
             layout = QVBoxLayout()
             layout.addWidget(self.username_input)
             layout.addWidget(self.password_input)
             layout.addWidget(self.button)
+            central_widget = QWidget()
             central_widget.setLayout(layout)
             self.setCentralWidget(central_widget)
-            #print(parent_instance.customers)
 
-            self.customer_window = None  # Initialize customer window as None
-
-        def open_window(self):
+        def authenticate(self):
             username = self.username_input.text()
             password = self.password_input.text()
-            window_instance = assignment1()
-            if username == "customer" and password=="12345":
-                self.customer_window = window_instance.customerWindow(window_instance)
+
+            if username in self.parent_instance.customers and password == self.parent_instance.customers[username]["password"]:
+                self.customer_window = Assignment1.CustomerWindow(self.parent_instance)
                 self.customer_window.show()
-            elif username == "admin1" and password == "12345":
-                self.adminID_window = window_instance.AdminIDWindow(window_instance)
-                self.adminID_window.show()
+            elif username in self.parent_instance.admins and password == self.parent_instance.admins[username]["password"]:
+                self.admin_id_window = Assignment1.AdminIDWindow(self.parent_instance)
+                self.admin_id_window.show()
             else:
-                self.error_window = assignment1.errorWindow(text="Error!\nInvalid username/password")
-                self.error_window.show()
+                QMessageBox.warning(self, "Error", "Invalid username/password!")
 
 # Run the application
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    #app_instance = assignment1()
-    #main_win = app_instance.AdminPortalWindow(app_instance) #Change to login once done
-    main_win = assignment1().MainWindow() #Change to login once done
+    app_instance = Assignment1()
+    main_win = app_instance.MainWindow(app_instance)
     main_win.show()
     sys.exit(app.exec_())
