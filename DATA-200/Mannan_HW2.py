10a2cceebf5a78e2b50d2098216e4e9621261610
Naeem Mannan <nmannan97@gmail.com>
2025-02-07 16:44:48 -0800

    Subtracting things

--- DATA-200/Mannan_HW2.py
+++ DATA-200/Mannan_HW2.py
@@ -1,18 +1,20 @@
-
+#Reservse class that will reverse the lines in a file
 class reverse:
 
     def __init__(self, file:str):
         self.file = file
         self.output = []
 
+    #Method that will reverse and sort the file
     def file_reverse(self):
 
         with open(self.file, 'r') as file:
             for line in file:
                 self.output.append(line.strip('\n') + "\n")        
             file.close()
+
         with open('result.txt', 'w') as file:
-            self.output.sort(reverse=True)
+            self.output = self.output[len(self.output)-1:0:-1]
             file.writelines(self.output)
             print(self.output)
             file.close()
