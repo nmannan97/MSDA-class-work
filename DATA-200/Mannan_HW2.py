a633c452faef3f0b858c63ca6fda33f88df1a480
Naeem Mannan <nmannan97@gmail.com>
2025-02-10 15:53:57 -0800

    adding hw2 for 200

--- DATA-200/Mannan_HW2.py
+++ DATA-200/Mannan_HW2.py
@@ -1,32 +1,51 @@
-#Reservse class that will reverse the lines in a file
-class reverse:
+import pandas as pd
 
-    def __init__(self, file:str):
-        self.file = file
-        self.output = []
+#Class for assignment 2
+class assignment2:
 
-    #Method that will reverse and sort the file
-    def file_reverse(self):
-        count = {'word':0, 'chars':0}
-        with open(self.file, 'r') as file:
-            self.output = file.read()
-            #print(self.output)
-            count['word'] = len(self.output.split())
-            #print(self.output)
-            for items in self.output:
-                for character in items:
-                    count['chars'] += 1
-            self.output = self.output.split('\n')
-            for index in range(len(self.output)):
-                self.output[index] = self.output[index] + '\n'
-            file.close()
+    class file_resvers:
+        def __init__(self, file:str):
+            self.file = file
+            self.output = []
 
-        with open('result.txt', 'w') as file:
-            self.output = self.output[len(self.output)-1:0:-1]
-            print("word count %d"%(count['word']))
-            print("Character count %d"%(count['chars']))
-            file.writelines(self.output)
-            file.close()
+        #Method that will reverse and sort the file
+        def file_reverse(self):
+            count = {'word':0, 'chars':0}
+            with open(self.file, 'r') as file:
+                self.output = file.read()
+                #print(self.output)
+                count['word'] = len(self.output.split())
+                #print(self.output)
+                for items in self.output:
+                    for character in items:
+                        count['chars'] += 1
+                self.output = self.output.split('\n')
+                for index in range(len(self.output)):
+                    self.output[index] = self.output[index] + '\n'
+                file.close()
 
+            with open('result.txt', 'w') as file:
+                self.output = self.output[len(self.output)-1:0:-1]
+                print("word count %d"%(count['word']))
+                print("Character count %d"%(count['chars']))
+                file.writelines(self.output)
+                file.close()
+    
+    class gdp:
 
-reverse("C:\\Users\\nmann\\OneDrive\\Desktop\\MSDA material\\MSDA-class-work\\DATA-200\\mary.txt").file_reverse() #input file herer
+        def __init__(self, csv:str):
+            self.df = pd.read_csv(csv)
+            pd.set_option('display.max_columns', None)
+            
+        def print_data(self):
+            #E:\Masters Data Analytics\MSDA-class-work\DATA-200
+            print(self.df)
+        
+        def get_data(self, country1: str, country2: str):
+            print(self.df[self.df['name'] == country1])
+            print(self.df[self.df['name'] == country2])
+
+#assignment2().file_resvers("C:\\Users\\nmann\\OneDrive\\Desktop\\MSDA material\\MSDA-class-work\\DATA-200\\mary.txt").file_reverse() #input file herer
+
+q4 = assignment2().gdp("E:\\Masters Data Analytics\\MSDA-class-work\\DATA-200\\Real GDP (purchasing power parity).csv")
+q4.get_data("China", "United States")
--- DATA-201/Assignment2.sql
+++ DATA-201/Assignment2.sql
@@ -1,15 +1,16 @@
+DROP TABLE employees;
+
 CREATE TABLE employees (
     id NUMBER PRIMARY KEY,
-    name_first VARCHAR2 (50) NOT NULL,
-    name_second VARCHAR2 (50) NOT NULL,
+    name_first VARCHAR2(50) NOT NULL,
+    name_second VARCHAR2(50) NOT NULL,
     unique_ids VARCHAR2(20)
-)
-SELECT us er FROM dual;
+);
 
 INSERT INTO employees(id, name_first, name_second, unique_ids)
-VALUES (1, "Charlamane", "Happsburg", " ");
+VALUES (1, 'Charlemagne', 'Happsburg', '0');
 
 INSERT INTO employees(id, name_first, name_second, unique_ids)
-VALUES (2, "Jane", "Doe", "UwU");
+VALUES (2, 'Jane', 'Doe', 'UwU');
 
-SELECT * FROM employees;
+SELECT * FROM system.employees;
