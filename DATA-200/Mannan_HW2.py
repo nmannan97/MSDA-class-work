408672f5b09ec5765e881bd046efebbf6b105737
Naeem Mannan <nmannan97@gmail.com>
2025-02-10 17:15:37 -0800

    adding hw2 for 200

--- DATA-200/Mannan_HW2.py
+++ DATA-200/Mannan_HW2.py
@@ -36,16 +36,29 @@ class assignment2:
         def __init__(self, csv:str):
             self.df = pd.read_csv(csv)
             pd.set_option('display.max_columns', None)
-            
+
+        #print data of the total CSV
         def print_data(self):
-            #E:\Masters Data Analytics\MSDA-class-work\DATA-200
             print(self.df)
         
+        #Get data of two countries (3)
         def get_data(self, country1: str, country2: str):
             print(self.df[self.df['name'] == country1])
             print(self.df[self.df['name'] == country2])
 
+        def get_combined_purchasing(self, *countries):
+            purchasing = 0
+            for items in countries:
+                temp = self.df[self.df['name'] == items]['value'].iloc[0]
+                #print(temp)
+                purchasing += int(temp.replace("$", "").replace(',', ""))
+            print(purchasing)
+
+        def delete_data(self, *countries):
+            pass
+
 #assignment2().file_resvers("C:\\Users\\nmann\\OneDrive\\Desktop\\MSDA material\\MSDA-class-work\\DATA-200\\mary.txt").file_reverse() #input file herer
 
 q4 = assignment2().gdp("E:\\Masters Data Analytics\\MSDA-class-work\\DATA-200\\Real GDP (purchasing power parity).csv")
-q4.get_data("China", "United States")
+#q4.get_data("China", "United States")
+q4.get_combined_purchasing("China", "United States","India")
