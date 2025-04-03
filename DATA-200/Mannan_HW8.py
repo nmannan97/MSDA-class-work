28c2e33be68460193be987ab41a9629b888142c0
Naeem Mannan <nmannan97@gmail.com>
2025-04-03 00:30:26 -0700

    adding finished product

--- DATA-200/Mannan_HW8.py
+++ DATA-200/Mannan_HW8.py
@@ -4,22 +4,30 @@ class quickSort:
     def __init__(self):
         self.array = [23, 6, 4, -1, 0, 12, 8, 3, 1]
 
-    def sort(self, List=[], i=0, j=0, pivot=0):
+    def sort(self, List=[], i=-1, j=0, pivot=0, count=0):
         pivot = len(List) - 1
-        if (List[j] <= List[pivot]):
+        if (len(List) - 1 == j):
+            print("Piviting at Len")
+            List[i + 1], List[j] = List[j], List[i + 1]
+            print(List)       
+            return self.sort(List, pivot=pivot)
+
+        elif (List[j] <= List[pivot]):
             print("Entering pivot")
-            temp = List[i]
-            List[i] = List[j]
-            List[j] = temp
-            if j == i:
-                j += 1
-            else:
-                j += 1
-                i += 1 
-            self.sort(List, i, j, pivot)
-        else:
-            print("RETURNING pivot")
-            return List
+            i += 1 
+            List[i], List[j] = List[j], List[i]
+            print(List)
+            print("i & j", i, j)
+            count += 1
+            if (count == len(List) - 1):
+                returnList = List
+                return returnList
+            return self.sort(List, i, j=j+1, pivot=pivot, count=count)
+
+        elif (List[j] > List[pivot]):
+            print("Entering pivot part B")
+            print(List)
+            return self.sort(List, i, j=j+1, pivot=pivot)
 
 if __name__ == "__main__":
     Sort = quickSort()
