221e8a36d8a14d1e5dc5bc2c3ec8398d2160f2b3
nmannan97 <nmannan97@gmail.com>
2025-10-01 14:39:11 -0700

    adding 226 work

---
+++ DATA-226/dags/lab1_dag.py
@@ -0,0 +1,5 @@
+from airflow import DAG
+from airflow.models import Variable
+from airflow.decorators import task
+from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+from datetime import datetime
